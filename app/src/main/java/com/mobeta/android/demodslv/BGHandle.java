1c74653aeac400bc55241c2e30319c2bf7b0da6e
xiaocaifly <rqhua_fly@163.com>
2018-02-08 00:05:31 +0800

    Initial commit

---
+++ app/src/main/java/com/mobeta/android/demodslv/BGHandle.java
@@ -0,0 +1,16 @@
+package com.mobeta.android.demodslv;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.rqhua.demo.dragsortlistview.R;
+
+public class BGHandle extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bg_handle_main);
+    }
+
+}
